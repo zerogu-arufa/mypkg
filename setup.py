705d6a9925d5edd611bd3570bab552dee00a124a
Tateuchi Naoya <kasutama59@gmail.com>
2023-12-22 23:09:46 +0900

    一から

--- setup.py
+++ setup.py
@@ -13,13 +13,15 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='zerog',
+    maintainer='Tateuchi Naoya',
     maintainer_email='kasutama59@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='a package for practice',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main', 
         ],
     },
 )
