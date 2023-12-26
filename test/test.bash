8003b8bddcdf8c2b02309ccca5ac7923788c9c2f
Tateuchi Naoya <kasutama59@gmail.com>
2023-12-26 21:18:24 +0900

    テストの修正

--- test/test.bash
+++ test/test.bash
@@ -8,7 +8,7 @@ colcon build
 
 source $dir/.bashrc
 
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 13 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'Listen: 10'
