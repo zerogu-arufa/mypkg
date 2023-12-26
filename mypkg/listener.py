a1f40bfa469f151cfc38b26542a0c1d59415dea2
Tateuchi Naoya <kasutama59@gmail.com>
2023-12-26 21:29:59 +0900

    listener.pyの改良

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -6,10 +6,14 @@ from std_msgs.msg import Int16
 class Listener(Node):
     def __init__(self):
         super().__init__("listener")
+        self.count = 0  # count属性を追加
         self.pub = self.create_subscription(Int16, "countup", self.cb, 10)
 
     def cb(self, msg):
         self.get_logger().info("Listen: %d" % msg.data)
+        self.count += 1
+        if self.count > 20:
+            rclpy.shutdown()
 
 def main(args=None):
     rclpy.init(args=args)
